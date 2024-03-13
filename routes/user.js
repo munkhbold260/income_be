dee87a4c2552b8348840a292ab40b267eadc8493
bold_max <munkhbold260@gmail.com>
2024-03-14 00:57:45 +0800

    add user 50%

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,30 @@
 const userRouter = require("express").Router();
-const { getUsers } = require("../service/user-service");
+const { getUsers, loginUser, signUp } = require("../service/user-service");
 
+/////////////////////////////////////////////////////////////////////
 userRouter.get("/user-get", async (req, res) => {
   const users = await getUsers();
-  res.json(users);
+  res.json(users); ////====>sending to frontend from data base
+  // console.log(users);
+});
+/////////////////////////////////////////////////////////////////////
+userRouter.post("/signin", async (req, res) => {
+  const user = req.body;
+  console.log("req.body", user);
+  const login = await loginUser();
+  res.json(login);
 });
 
-userRouter.post("/signin", async (req, res) => {});
+/////////////////////////////////////////////////////////////////////
+userRouter.post("/signup", async (req, res) => {
+  const newUserData = req.body;
+  // console.log("newUserDatanewUserData", newUserData);
+  const result = await signUp(newUserData);
+  res.json(result);
+  console.log("reeesssuuult", result);
+});
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////
 
 module.exports = userRouter;
--- service/user-service.js
+++ service/user-service.js
@@ -14,7 +14,7 @@ const pgConif = {
 };
 
 const pool = new Pool(pgConif);
-
+/////////////////////////////////////////////////////////////////////
 async function getUsers() {
   const client = await pool.connect();
   let response;
@@ -25,7 +25,51 @@ async function getUsers() {
   } finally {
     client.release();
   }
+  // console.log(response.rows);
+  return response.rows; /// =====> data base-s irseniig routeruu ilgeej bn
+}
+/////////////////////////////////////////////////////////////////////
+async function loginUser(req, res) {
+  console.log("uusseeerrrrrr", res);
+  // // const user = res.body;
+  // const client = await pool.connect();
+  // let response;
+  // const Query = `SELECT * FROM users WHERE (email='${user.email}' AND password='${user.password}');`;
+  // // const Query = `INSERT INTO users (name,  email, id ,password) VALUES ('${user.email}','${user.password}'); `;
+  // try {
+  //   response = await client.query(Query);
+  //   const dbResponse = await client.query(Query);
+  //   const result = res.send(dbResponse.rows[0]);
+  //   if (dbResponse["rowCount"]) {
+  //     return res.status(200).send({ success: result });
+  //   } else {
+  //     return res.status(500).send({ success: "false" });
+  //   }
+  // } catch (e) {
+  //   console.log(e);
+  // } finally {
+  //   client.release();
+  //   console.log("signin successfully");
+  // }
+}
+/////////////////////////////////////////////////////////////////////
+async function signUp(newUser) {
+  const client = await pool.connect();
+  let response;
+  try {
+    response = await client.query(
+      "INSERT INTO users (name, email, password,id) VALUES ($1, $2, $3, $4)",
+      [newUser.name, newUser.email, newUser.password, newUser.id]
+    );
+  } catch (error) {
+    throw new Error(error ? error.message : "Error");
+  } finally {
+    client.release();
+  }
   return response.rows;
 }
+/////////////////////////////////////////////////////////////////////
 
-module.exports = { getUsers };
+/////////////////////////////////////////////////////////////////////
+
+module.exports = { getUsers, loginUser, signUp };
